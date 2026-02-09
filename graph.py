220c0bb7b43b853929de62183a42a2dc26611fd7
Gregoire <gregoire.boucheroy@efrei.net>
2026-02-09 23:20:24 +0100

    Ajout des fonctionnalités get_successors et get_predecessors

--- graph.py
+++ graph.py
@@ -13,9 +13,24 @@ class Graph:
             self.add_vertex(end)
         self.graph[start].append((end, weight))
 
+    def get_successors(self, vertex):
+        if vertex not in self.graph:
+            return []
+        else:
+            return [successor for successor, weight in self.graph[vertex]]
+
+    def get_predecessors(self, vertex):
+        predecessors = []
+        if vertex not in self.graph:
+            return []
+        else:
+            for predecessor in self.graph:
+                if vertex in self.get_successors(predecessor):
+                    predecessors.append(predecessor)
+        return predecessors
+
     def __str__(self):
         result = ""
         for vertex, successors in self.graph.items():
             result += f"{vertex} -> {successors}\n"
         return result
-
