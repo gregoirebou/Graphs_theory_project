4c562d1df0832caefdbe592900f5cf858777ef80
Gregoire <gregoire.boucheroy@efrei.net>
2026-02-09 20:49:49 +0100

    Ajout de la fonction __str__

--- graph.py
+++ graph.py
@@ -2,15 +2,20 @@ class Graph:
     def __init__(self):
         self.graph = {}
 
-        def add_vertex(self, vertex):
-            if vertex not in self.graph:
-                self.graph[vertex] = []
+    def add_vertex(self, vertex):
+        if vertex not in self.graph:
+            self.graph[vertex] = []
 
-        def add_edge(self, start, end, weight=1):
-            if start not in self.graph:
-                self.add_vertex(start)
-                self.graph[start].append(end)
-            else:
-                if end not in self.graph[start]:
-                    self.graph[start].append(end)
+    def add_edge(self, start, end, weight=1):
+        if start not in self.graph:
+            self.add_vertex(start)
+        if end not in self.graph:
+            self.add_vertex(end)
+        self.graph[start].append((end, weight))
+
+    def __str__(self):
+        result = ""
+        for vertex, successors in self.graph.items():
+            result += f"{vertex} -> {successors}\n"
+        return result
 
